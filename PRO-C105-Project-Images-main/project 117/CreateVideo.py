7a5b43a26c5247dbce9e7985823d310c6ce17085
ksps7722 <88386473+ksps7722@users.noreply.github.com>
2022-08-04 19:08:28 +0530

    Add files via upload

---
+++ PRO-C105-Project-Images-main/project 117/CreateVideo.py
@@ -0,0 +1,35 @@
+import os
+from tkinter import Frame, Image
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        images.append(file_name)
+        
+print(len(images))
+count = len(images)
+frame=cv2.imread(images[0])
+height,width,channels=frame.shape
+size=(width,height)
+out=cv2.VideoWriter("my project 117.mp4",cv2.VideoWriter_fourcc(*'DIVX'),15,size)
+
+##for sunset the code is :for i in range(0,count-1)
+##the same code for the sunset line number 30 to 35
+for i in range(count-1,0,-1):
+    frame=cv2.imread(images[i])
+    out.write(frame)
+
+out.release()
+print("done")
